af4fc9d1a4dd97dfb8bc608a5311a68a33d2a62f
Leonardo Falanga <info@leonardofalanga.it>
2024-11-26 16:43:20 +0100

    Red phase use story 1 & 2

--- src/smart_room.py
+++ src/smart_room.py
@@ -47,20 +47,13 @@ class SmartRoom:
         self.fan_on = False
 
     def check_room_occupancy(self) -> bool:
-        if GPIO.input(self.INFRARED_PIN):
-            return True
-        pass
+        return GPIO.input(self.INFRARED_PIN)
 
     def check_enough_light(self) -> bool:
-        if self.bmp280_indor.read_light() > 1000:
-            return True
-        pass
+        return GPIO.input(self.PHOTO_PIN)
 
     def manage_light_level(self) -> None:
-        if self.sensair.read_co2() > 1000:
-            self.light_on = True
-            self.window_open = True
-            self.fan_on = True
+        # To be implemented
         pass
 
     def manage_window(self) -> None:
--- test/test_smart_room.py
+++ test/test_smart_room.py
@@ -8,27 +8,20 @@ from src.smart_room import SmartRoom
 from mock.senseair_s8 import SenseairS8
 
 
+
 class TestSmartRoom(unittest.TestCase):
 
     @patch.object(GPIO, "input")
-    def SmartRoom.check_room_occupancy(self, mock_infrared_sensor: Mock):
+    def test_check_room_occupancy(self, mock_infrared_sensor: Mock):
         mock_infrared_sensor.return_value = True
         system = SmartRoom()
-        occupied = system.check_room_occupancy(system.INFRARED_PIN)
+        occupied = system.check_room_occupancy()
         self.assertTrue(occupied)
 
-    @patch.object(Adafruit_BMP280_I2C, "__init__")
-    def SmartRoom.check_enough_light(self, mock_bmp280: Mock):
-        mock_bmp280.return_value = True
+    @patch.object(GPIO, "input")
+    def test_check_enough_light(self, mock_light_sensor: Mock):
+        mock_light_sensor.return_value = True
         system = SmartRoom()
-        light = system.check_enough_light()
-        self.assertTrue(light)
+        enough_light = system.check_enough_light()
+        self.assertTrue(enough_light)
 
-    @patch.object(Senseair, "__init__")
-    def SmartRoom.manage_light_level(self, mock_sensair: Mock):
-        mock_sensair.return_value = True
-        system = SmartRoom()
-        system.manage_light_level()
-        self.assertTrue(system.light_on)
-        self.assertTrue(system.window_open)
-        self.assertTrue(system.fan_on)
